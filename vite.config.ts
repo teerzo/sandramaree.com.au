e0d1ae1526494067d01ef61b53577778c8447241
Kyle <teerzo@live.com.au>
2026-01-09 02:29:49 +1100

    fixing vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
+
 
 const config = defineConfig({
   plugins: [
@@ -13,7 +15,7 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    nitro(),
   ],
+  nitro: {},
 })
-
-export default config
